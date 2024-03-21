654067ea941a725aaf96342deb64e4437be946f4
adamd <adam.d.hinton@gmail.com>
2024-03-20 23:48:24 -0400

    Challenge specs

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,15 +1,31 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+package dev.lpa;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
+
+// Challenge specs
+
+// class Theatre
+    // Three fields?
+        // theatreName
+        // seatsInRow int
+        // field for the seats themselves
+            // TreeSet
+    // Constructor:
+        // theatre name, numRows, numSeats total in theater
+        // Assume uniform seats per row; numRows should never exceed 26 so will be labeled A-Z
+    // Create the seats, and number them, as part of initialization of Theatre class
+    // Should also have printSeatMap method that prints each seat, with seat row printed on a separate line.
+    // Allow a booking agent to reserve a single seat, and seat map shows which ones are reserved.
+
+
+    // has set of seats
+    // Seat class should be nested class on Theatre
+        // Seat constructed with row char and a seat number integer
+        // Each seat has a string - a seat number - in the format "A005", where A is the row number, and 005 is seat
+        // %number. Zero padded up to three digits.
+        // Seat also has isReserved boolean
+
