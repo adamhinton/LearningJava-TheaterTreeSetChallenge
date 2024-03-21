66c045471bad117828e8b0b77b6a16f3fa6d2677
adamd <adam.d.hinton@gmail.com>
2024-03-20 23:55:29 -0400

    flesh out Theatre.java

--- src/dev/lpa/Theatre.java
+++ src/dev/lpa/Theatre.java
@@ -1,31 +1,52 @@
 package dev.lpa;
 
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
 public class Theatre {
+
+    private String theatreName;
+    // uniform in all rows
+    private int seatsInRow;
+    private NavigableSet<Seat> seats = new TreeSet<>();
+
+    public Theatre(String theatreName, int seatsInRow) {
+        this.theatreName = theatreName;
+        this.seatsInRow = seatsInRow;
+    }
+
+    class Seat {
+
+    }
+
+        // has set of seats
+    // Seat class should be nested class on Theatre
+        // Seat constructed with row char and a seat number integer
+        // Each seat has a string - a seat number - in the format "A005", where A is the row number, and 005 is seat
+        // %number. Zero padded up to three digits.
+        // Seat also has isReserved boolean
+
+
+
 }
+
 
 
 
 // Challenge specs
 
 // class Theatre
-// Three fields?
-// theatreName
-// seatsInRow int
-// field for the seats themselves
-// TreeSet
-// Constructor:
-// theatre name, numRows, numSeats total in theater
-// Assume uniform seats per row; numRows should never exceed 26 so will be labeled A-Z
-// Create the seats, and number them, as part of initialization of Theatre class
-// Should also have printSeatMap method that prints each seat, with seat row printed on a separate line.
-// Allow a booking agent to reserve a single seat, and seat map shows which ones are reserved.
+    // Three fields?
+        // theatreName
+        // seatsInRow int
+        // field for the seats themselves
+            // TreeSet
+    // Constructor:
+        // theatre name, numRows, numSeats total in theater
+        // Assume uniform seats per row; numRows should never exceed 26 so will be labeled A-Z
+    // Create the seats, and number them, as part of initialization of Theatre class
+    // Should also have printSeatMap method that prints each seat, with seat row printed on a separate line.
+    // Allow a booking agent to reserve a single seat, and seat map shows which ones are reserved.
 
-
-// has set of seats
-// Seat class should be nested class on Theatre
-// Seat constructed with row char and a seat number integer
-// Each seat has a string - a seat number - in the format "A005", where A is the row number, and 005 is seat
-// %number. Zero padded up to three digits.
-// Seat also has isReserved boolean
 
 
